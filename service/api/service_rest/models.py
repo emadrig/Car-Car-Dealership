dd99fd5add140c0cb4fedba2c9ba9bcc93fce961
Edwin <emadrig1@uci.edu>
2023-01-23 14:13:38 -0800

    updated the service_rest models to have a AutomobileVO model and created the poller to get Automobile Data from the inventory database

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Technician(models.Model):
+  name = models.CharField(max_length=50)
+  employee_number = models.CharField(max_length=50, unique=True)
+
+
+# class Appointment(models.Model):
+#   owner_name = models.CharField(max_length=100)
+#   date_time = models.DateTimeField()
+#   reason= models.TextField()
+
+#   technician = models.ForeignKey(
+#     Technician,
+#     related_name="apointments",
+#     on_delete=models.CASCADE,
+#   )
+
+
+class AutomobileVO(models.Model):
+  import_href =models.CharField(max_length=200, unique=True)
+  vin = models.CharField(max_length=17)
