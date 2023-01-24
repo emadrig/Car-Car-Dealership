8f72ce575febde824fb42952fa7473e9e4a047fc
jpcodes <Snoahy0308>
2023-01-24 06:56:46 -0500

    Initial commit, poller working

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class AutoVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    vin = models.CharField(max_length=17, unique=True)
+    is_sold = models.BooleanField(default=False)
--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -4,11 +4,20 @@ import sys
 import time
 import json
 import requests
-
+from sales_rest.models import AutoVO
 sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_project.settings")
 django.setup()
 
+def get_inventory():
+    response = requests.get("http://inventory-api:8000/api/automobiles")
+    content = json.loads(response.content)
+    print(content["autos"])
+    for auto in content["autos"]:
+        AutoVO.objects.update_or_create(
+            import_href=auto["href"],
+            defaults={"vin": auto["vin"]}
+        )
 # Import models from sales_rest, here.
 # from sales_rest.models import Something
 
@@ -17,10 +26,11 @@ def poll():
         print('Sales poller polling for data')
         try:
             # Write your polling logic, here
-            pass
+            get_inventory()
         except Exception as e:
+            print("ERROR")
             print(e, file=sys.stderr)
-        time.sleep(60)
+        time.sleep(10)
 
 
 if __name__ == "__main__":
